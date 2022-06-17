f1d562d8fad686ae983430cee34e5fd5f0c0d01f
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-06-17 22:39:09 +0530

    doc: header added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import TaskList from './components/TaskList'
 import './App.css'
 import TaskListContextProvider from './context/TaskListContext'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <TaskListContextProvider>
       <div className='container'>
         <div className='app-wrapper'>
+          <Header />
           <div className='main'></div>
         <TaskList />
         </div>
-
       </div>
     </TaskListContextProvider>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/TaskHub.png';
+
+const Header = () => {
+    return (
+        <div style={{display:'flex', color:'white', justifyContent: 'space-between'}}>
+            <h1>Task-Hub</h1>
+            <img src={logo} alt="" style={{ width: '40px', borderRadius: '10px' }} />
+        </div>
+    )
+}
+
+export default Header;
